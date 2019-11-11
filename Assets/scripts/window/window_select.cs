1ea1a5a913b3c54d74ca375921cc728ce538f9ab
komejisatori <thss15_wangzr@163.com>
2019-11-11 21:28:43 +0800

    windows overlap #3

--- Assets/scripts/window/window_select.cs
+++ Assets/scripts/window/window_select.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class window_select : MonoBehaviour
+public class window_select : MonoBehaviour, IComparer<window_select>
 {
     // Start is called before the first frame update
     public bool m_activate;
+    public int m_z_index; // windows init order
     private BoxCollider2D[] m_colliders;
+    
     void Start()
     {
+        this.SetZIndex();
         m_colliders = this.GetComponentsInChildren<BoxCollider2D>();
         if (!m_activate)
         {
@@ -28,16 +31,43 @@ public class window_select : MonoBehaviour
 
     void OnMouseDown()
     {
-        Debug.Log("click");
         if (!m_activate)
         {
-            Debug.Log("click");
             m_activate = true;
             foreach (BoxCollider2D collider in m_colliders)
             {
                 if (!collider.isTrigger)
                     collider.enabled = true;
             }
+        }
+        window_stack stack = window_stack.GetInstance();
+        stack.PopWindow(m_z_index);
+    }
+
+    public void SetZIndex()
+    {
+        windows_frame[] sons = this.GetComponentsInChildren<windows_frame>();
+        windows_body body = this.GetComponentInChildren<windows_body>();
+        foreach (windows_frame son in sons)
+        { 
+            son.transform.position = new Vector3(son.transform.position.x, son.transform.position.y, m_z_index * 10);
+        }
+        body.transform.position = new Vector3(body.transform.position.x, body.transform.position.y, m_z_index * 10 + 1);
+    }
+
+    public int Compare(window_select a, window_select b)
+    {
+        if (a.m_z_index > b.m_z_index)
+        {
+            return 1;
+        }
+        else if (a.m_z_index < b.m_z_index)
+        {
+            return -1;//小的放右边
+        }
+        else
+        {
+            return 0;//不变
         }
     }
 }
---
+++ Assets/scripts/window/window_stack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class window_stack
+{
+    // Start is called before the first frame update
+    private List<window_select> m_windows_stack ;
+    private static window_stack instance;
+    private window_stack()
+    {
+        window_select[] windows = UnityEngine.Object.FindObjectsOfType<window_select>();
+        Debug.Log(windows.Length);
+        m_windows_stack = new List<window_select>(windows);
+        m_windows_stack.Sort(new window_select());
+    }
+    public static window_stack GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new window_stack();
+        }
+        return instance;
+    }
+
+    public void PopWindow(int z_index)
+    {
+        window_select w_temp = m_windows_stack[z_index];
+        for (int i = z_index; i > 0; i--)
+        {
+            m_windows_stack[i] = m_windows_stack[i-1];
+            m_windows_stack[i].m_z_index = i;
+            m_windows_stack[i].SetZIndex();
+        }
+        m_windows_stack[0] = w_temp;
+        m_windows_stack[0].m_z_index = 0;
+        m_windows_stack[0].SetZIndex();
+    }
+
+}
+
