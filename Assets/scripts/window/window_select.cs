8c4f1fbd8dc22a64b63eff5126c6f3672b0044ef
komejisatori <thss15_wangzr@163.com>
2019-11-11 16:41:58 +0800

    init window collider

---
+++ Assets/scripts/window/window_select.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class window_select : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool m_activate;
+    private BoxCollider2D[] m_colliders;
+    void Start()
+    {
+        m_colliders = this.GetComponentsInChildren<BoxCollider2D>();
+        if (!m_activate)
+        {
+            foreach (BoxCollider2D collider in  m_colliders)
+            {
+                if (!collider.isTrigger)
+                    collider.enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("click");
+        if (!m_activate)
+        {
+            Debug.Log("click");
+            m_activate = true;
+            foreach (BoxCollider2D collider in m_colliders)
+            {
+                if (!collider.isTrigger)
+                    collider.enabled = true;
+            }
+        }
+    }
+}
