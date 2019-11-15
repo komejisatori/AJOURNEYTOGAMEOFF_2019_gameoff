bdc3a1a5824ceee31f15b0352ec5251cb8a1ec3f
komejisatori <thss15_wangzr@163.com>
2019-11-16 03:40:48 +0800

    windows overlap adjustment #5

--- Assets/scripts/avatar/avatar_shiftwindow.cs
+++ Assets/scripts/avatar/avatar_shiftwindow.cs
@@ -10,12 +10,12 @@ public class avatar_shiftwindow : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // Update is called once per fram0.1
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            Collider2D[] hitColliders = Physics2D.OverlapBoxAll(this.transform.position, new Vector2(1, 1), 0);
+            Collider2D[] hitColliders = Physics2D.OverlapBoxAll(this.transform.position, new Vector2(0.1f, 0.1f), 0);
             foreach (Collider2D hitc in hitColliders)
             {
                 window_select window = hitc.GetComponentInParent<window_select>();
